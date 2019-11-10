096d3debfad2c562b37f115fa8d9dda58e951a55
Jully Nobre <jullynobre1@gmail.com>
2019-11-10 18:49:51 -0300

    Add 'Star' button to BookDetails

--- books-list/books-list/ViewControllers/BookDetailsViewController.swift
+++ books-list/books-list/ViewControllers/BookDetailsViewController.swift
@@ -13,11 +13,14 @@ class BookDetailsViewController: UIViewController {
     private var bookDetailsView: BookDetailsView?
     private var item: Item?
     
+    private var indexInStaerdBooks: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUpView()
         loadViewData()
+        configureBarButton()
     }
     
     private func setUpView() {
@@ -29,6 +32,7 @@ class BookDetailsViewController: UIViewController {
         self.bookDetailsView?.bookTitle.text = self.item?.volumeInfo.title
         self.bookDetailsView?.bookDescription.text = self.item?.volumeInfo.description ?? "This book has no description."
         
+        //Load Image
         let imageURL = self.item?.volumeInfo.imageLinks?.large ?? self.item?.volumeInfo.imageLinks?.medium ?? self.item?.volumeInfo.imageLinks?.thumbnail ?? ""
         Request<BooksAPI>().loadImage(from: imageURL) { (image, error) in
             if image != nil {
@@ -39,7 +43,37 @@ class BookDetailsViewController: UIViewController {
         }
     }
     
+    private func configureBarButton() {
+        let starredBooks = UserDefaults.standard.stringArray(forKey: "StaredBooks")
+        self.indexInStaerdBooks = starredBooks!.firstIndex(of: item!.id)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: self.indexInStaerdBooks != nil ? "Unstar" : "Star",
+            style: .plain, target: self,
+            action: #selector(starButtonTapped)
+        )
+        
+    }
+    
     func setItem(item: Item) {
         self.item = item
+    }
+    
+    @objc func starButtonTapped() {
+        var starredBooks = UserDefaults.standard.stringArray(forKey: "StaredBooks")
+        //Stars the book and save to User Defaults
+        if self.indexInStaerdBooks == nil {
+            starredBooks?.append(self.item!.id)
+            self.indexInStaerdBooks = starredBooks!.count - 1
+            UserDefaults.standard.set(starredBooks, forKey: "StaredBooks")
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Unstar", style: .plain, target: self, action: #selector(starButtonTapped))
+        }
+        //Unstars the book and save to User Defaults
+        else {
+            starredBooks?.remove(at: self.indexInStaerdBooks!)
+            self.indexInStaerdBooks = nil
+            UserDefaults.standard.set(starredBooks, forKey: "StaredBooks")
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Star", style: .plain, target: self, action: #selector(starButtonTapped))
+        }
     }
 }
